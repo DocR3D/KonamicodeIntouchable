64dcdbb18bf68c1dd363dabc6fd785baef55d1a4
DocR3D <yonnet.maxime@laposte.net>
2017-12-08 04:48:45 +0100

    Updated all the command available

--- KonamiDone/js/index.js
+++ KonamiDone/js/index.js
@@ -28,16 +28,20 @@ function returnResponse() {
   
   // plain command vs starting with sudo
   var answer = getInput();
-  var answer2 = "To have all the help write \'help\'";
+  var answer2 = " Sorry Didn't have the time to think about this, To have all the commands available write \'help\'";
   if(answer=="help") answer2="Commands lists : help, win, sudo, sources, more, ls, credits'";
   if(answer=="more") answer2="Use more to load a file";
+  if(answer=="win") answer2="Haha It would be too easy";
   if(answer=="ls") answer2="Answer, useless, ..,";
+  if(answer==":(") answer2="Don't be sad";
+  if(answer==":)") answer2="=)"; 
+  if(answer=="clear") answer2="Nope that don't work sorry"; 
   if(answer=="more Answer") answer2="You found the answer, write \"Nuit2info2017\"";
-  if(answer=="more useless") answer2="Nope that's useless.";
+  if(answer=="more useless") answer2="Nope that's really useless.";
   if(answer=="sudo") answer2="You already have all the rights";
   if(answer=="sources") answer2="Thanks to Giana  for these scripts, you can find it here : https://codepen.io/giana/pen/XXzQOW"; 
   if(answer=="Nuit2info2017") window.location.href = "../victory/index.html";
-  if(answer=="credits") answer2="Maxime Yonnet - Intouchable";
+  if(answer=="credits") answer2="Maxime Yonnet - Damien Robert - Intouchable";
   
   li.textContent = answer2;
 
